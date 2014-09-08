6b57c58e5b9e685383e93f912f1a6ea4f0f5923f
Deivid Marques <deividmarques@gmail.com>
2014-09-08 19:15:06 -0300

    small fixes

--- source/assets/javascripts/docs/_example.js
+++ source/assets/javascripts/docs/_example.js
@@ -3,6 +3,10 @@ var lsexample = (function() {
 
   function init() {
     activeMenu();
+    disabledClient();
+    addLoose();
+    doGetItem();
+    doClear();
   }
 
   function activeMenu() {
@@ -10,6 +14,42 @@ var lsexample = (function() {
     $itemActive.parents('.ls-submenu').children('a').trigger('click');
     $itemActive.parent('li').addClass('ls-active');
   }
+
+  function disabledClient(){
+    $('#disabledClient').on('click',function(){
+      $('#feedbackDisabled').show();
+    });
+  }
+
+  function addLoose(){
+    $('#addLoose').on('click',function(){
+      var inputLoose = $('#inputLoose').val();
+      doSetItem();
+      $('#addMessage').hide();
+      $('.areaLoose').html(inputLoose);
+      $('#feedbackLoose').show();
+    });
+  }
+
+  function doSetItem(){
+    var myPost = document.querySelector('#inputLoose').value;
+    localStorage.setItem('texto', myPost);
+  }
+
+  function doGetItem() {
+    var key = localStorage.getItem('texto');
+    $('.areaLoose').html(key);
+  }
+
+  function doClear() {
+    $('[data-target="#removeMessage"]').on('click',function(){
+      $('.areaLoose').html('0');
+      localStorage.setItem('texto', '0');
+    })
+  }
+
+
+
 
   return {
     init:init
